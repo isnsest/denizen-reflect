0cd8fdbe5dfd4e05e5ce3098a581a6258a9d4167
loyslow <gagagaopo@gmail.com>
2026-01-16 18:27:34 +0200

    Bump version to 2.2.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
 
 val buildNumber: String = System.getenv("BUILD_NUMBER") ?: project.property("BUILD_NUMBER") as String
 val buildDate: String = SimpleDateFormat("ddMMyyyy").format(Date())
-val pluginVersion = "2.2.3"
+val pluginVersion = "2.2.4"
 
 group = "isnsest"
 version = pluginVersion
